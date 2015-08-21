28c3194bc02d1f015b8815e67690ded07010a14e
Yusuke Endoh <mame@ruby-lang.org>
2015-08-22 03:26:54 +0900

    * try travis ci docker.

--- src/dot.travis.yml.gen.rb
+++ src/dot.travis.yml.gen.rb
@@ -5,16 +5,12 @@ apts = RunSteps.flat_map {|s| s.apt }
 other_packages = %w(cmake libpng12-dev libgd2-xpm-dev groff tcc)
 
 
-utopic_to_precise = {
-  "clojure1.6" => "clojure",
-  "emacs24" => "emacs23",
-  "gambas3-script" => "gambas2-script",
-}
-
-apts = apts.flatten.map {|apt| utopic_to_precise[apt] || apt }.compact
+apts = apts.flatten.map {|apt| apt }.compact
 apts.delete("ruby2.1")
 
 yaml = {}
+yaml["sudo"] = "required"
+yaml["services"] = ["docker"]
 yaml["language"] = "ruby"
 yaml["rvm"] = ["2.1.0"]
 yaml["env"] = ["PATH=/usr/games:$PATH"]
@@ -24,11 +20,6 @@ yaml["before_install"] = [
   "sudo apt-get update",
   "sudo apt-get remove postgresql-common -y --purge",
   'sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade',
-  "sudo add-apt-repository ppa:ecere-team/ppa -y",
-  "sudo add-apt-repository ppa:directhex/ppa -y",
-  "sudo add-apt-repository ppa:staticfloat/juliareleases -y",
-  "sudo add-apt-repository ppa:staticfloat/julia-deps -y",
-  "sudo add-apt-repository ppa:octave/stable -y",
   "sudo apt-get update",
   'sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade',
 ]
